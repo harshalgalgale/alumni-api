cce8bb67a5dea07e5f4e55e18a0253432da50b85
harshalgalgale <h.galgale@gmail.com>
2021-08-04 19:23:16 +0100

    modifications to settings file

--- alumni/settings.py
+++ alumni/settings.py
@@ -10,27 +10,82 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_file = os.path.join(BASE_DIR,  ".env")
+
+# If a local .env doesn't exist, create one by loading it from Secret Manager.
+if not os.path.isfile(env_file):
+    import google.auth
+    from google.cloud import secretmanager_v1 as sm
+
+    _, project = google.auth.default()
+
+    if project:
+        client = sm.SecretManagerServiceClient()
+        settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+        name = f"projects/{project}/secrets/{settings_name}/versions/latest"
+        payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+        with open(env_file, "w") as f:
+            f.write(payload)
+
+env = environ.Env()
+env.read_env(env_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lus#8o^0++^1t&8t9k+x(%rj^i0$gk*klpf@txn83537mceux'
+# SECRET_KEY = 'django-insecure-0lus#8o^0++^1t&8t9k+x(%rj^i0$gk*klpf@txn83537mceux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+root = environ.Path(__file__) - 3
+SITE_ROOT = root()
+
+DEBUG = env("DEBUG", default=False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+if "CURRENT_HOST" in os.environ:
+    # handle raw host(s), or http(s):// host(s), or no host. 
+    HOSTS = []
+    for h in env.list("CURRENT_HOST"):
+        if "://" in h:
+            h = h.split("://")[1]
+        HOSTS.append(h)
+else:
+    # Assume localhost if no CURRENT_HOST
+    HOSTS = ["localhost"]
+
+ALLOWED_HOSTS = ["127.0.0.1"] + HOSTS
+
+# Enable Django security precautions if *not* running locally
+if "localhost" not in ALLOWED_HOSTS:
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = "DENY"
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +93,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS =[
+    # 'rest_framework',
+    # 'rest_framework.authtoken',
+    # 'corsheaders',
+    # 'import_export',
+    # 'drf_yasg',
+    # 'django_extensions',
+]
+
+PROJECT_APPS = [
+    'accounts',
+    # 'core',
+    # 'students',
+    # 'members',
+    # 'article',
+    # 'committee',
+    # 'careers',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +149,7 @@ WSGI_APPLICATION = 'alumni.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {"default": env.db()}
 
 
 # Password validation
@@ -104,22 +175,38 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+GS_BUCKET_NAME = env("GS_BUCKET_NAME", default=None)
+
+if GS_BUCKET_NAME: 
+    DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+    STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+    GS_DEFAULT_ACL = "publicRead"
+
+    INSTALLED_APPS += ["storages"]
+
+else:
+    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    STATIC_URL = STATIC_ROOT
+
+    MEDIA_ROOT = "media/"  # where files are stored on the local filesystem
+    MEDIA_URL = "/media/"  # what is prepended to the image URL
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
+AUTH_USER_MODEL = "accounts.User"
+
