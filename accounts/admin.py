bf60c91898cf4f2f2d523fab8e71e65aecb5cc41
harshalgalgale <h.galgale@gmail.com>
2021-08-05 23:27:29 +0100

    modified user admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
+
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ["username", "name", "is_superuser", "last_login"]
+    search_fields = ["name", "username", "email"]
+
 
 admin.site.register(User, UserAdmin)
