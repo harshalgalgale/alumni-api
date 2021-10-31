87a6b1b40eb403fbd38f77d392212438b0bcbd28
harshalgalgale <h.galgale@gmail.com>
2021-10-31 14:28:30 +0000

    Added defaults to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,22 @@
 variable project {
   type        = string
   description = "The Google Cloud Platform project name"
+  default = "alumni-dascaet-rahuri"
 }
 
 variable service {
   description = "Name of the service"
   type        = string
+  default = "alumni-api"
 }
 
 variable region {
-  default = "us-central1"
+  default = "europe-west2"
   type    = string
 }
 
 variable instance_name {
   description = "Name of the postgres instance (PROJECT_ID:REGION:INSTANCE_NAME))"
   type        = string
+  default = "alumni-dascaet-rahuri:europe-west2:alumni-db-psql"
 }
